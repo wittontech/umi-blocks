42f7798af2ad750531a72f9d586d3e1ccf085aec
Witton Zhao <wendeng.zhao@sayweee.com>
2020-08-07 15:47:53 +0800

    Initial repo with Horizon block

---
+++ Horizon/src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './index.css';
+
+const Horizon: React.FC = () => {
+  return (
+    <div className={styles.normal}>
+      <h1>Horizon - Dream Start Here!</h1>
+    </div>
+  );
+};
+
+export default Horizon;
